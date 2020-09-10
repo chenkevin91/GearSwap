b411b0389218f81b9daef36f7a19fd61f4f9a544
Kevin Chen <chen.kevin91@gmail.com>
2020-09-10 16:07:16 -0400

    Initial Commit

---
+++ GearSwap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GearSwap
+//
+//  Created by Kevin Chen on 9/10/20.
+//  Copyright © 2020 Kevin Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
